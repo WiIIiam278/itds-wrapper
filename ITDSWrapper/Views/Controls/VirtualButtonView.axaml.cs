2aa5676b1cc80264491d94bf399c3817c8cc1167
jonko0493 <jonko0493@protonmail.com>
2025-09-10 17:05:53 -0700

    Continue working on this stuff

--- ITDSWrapper/Views/Controls/VirtualButtonView.axaml.cs
+++ ITDSWrapper/Views/Controls/VirtualButtonView.axaml.cs
@@ -1,6 +1,5 @@
 using Avalonia.Controls;
 using Avalonia.Input;
-using ITDSWrapper.Input;
 using ITDSWrapper.ViewModels.Controls;
 
 namespace ITDSWrapper.Views.Controls;
@@ -10,17 +9,18 @@ public partial class VirtualButtonView : UserControl
     public VirtualButtonView()
     {
         InitializeComponent();
-        InputButton.AddHandler(PointerPressedEvent, Button_OnPointerPressed, handledEventsToo: true);
-        InputButton.AddHandler(PointerReleasedEvent, Button_OnPointerReleased, handledEventsToo: true);
+        InputButton.AddHandler(PointerEnteredEvent, Button_OnPointerEntered, handledEventsToo: true);
+        InputButton.AddHandler(PointerExitedEvent, Button_OnPointerExited, handledEventsToo: true);
     }
 
-    private void Button_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    private void Button_OnPointerEntered(object? sender, PointerEventArgs e)
     {
         ((VirtualButtonViewModel)DataContext!).AssociatedInput?.Press((VirtualButtonViewModel)DataContext!);
     }
-
-    private void Button_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    
+    private void Button_OnPointerExited(object? sender, PointerEventArgs e)
     {
         ((VirtualButtonViewModel)DataContext!).AssociatedInput?.Release((VirtualButtonViewModel)DataContext!);
     }
+
 }
