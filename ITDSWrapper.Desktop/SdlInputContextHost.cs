a066f28cc94b1a0679db99e8ffa84265fe37eecd
jonko0493 <jonko0493@protonmail.com>
2026-05-17 02:12:51 +0900

    Remove EventDriven

--- ITDSWrapper.Desktop/SdlInputContextHost.cs
+++ ITDSWrapper.Desktop/SdlInputContextHost.cs
@@ -51,8 +51,6 @@ public sealed class SdlInputContextHost : IDisposable
         {
             View.Initialize();
         }
-
-        View.IsEventDriven = true;
     }
 
     public void Dispose()
