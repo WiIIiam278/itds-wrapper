10a0cee707828471c782e9267f65f53993a39a9d
jonko0493 <jonko0493@protonmail.com>
2026-07-25 01:57:00 +0900

    Attempt 1

--- ITDSWrapper.iOS/Main.cs
+++ ITDSWrapper.iOS/Main.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Libretro.NET.Bindings;
 using UIKit;
 
 namespace ITDSWrapper.iOS;
@@ -7,8 +11,18 @@ public class Application
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
+        // Set up resolver path for melondsds_libretro
+        NativeLibrary.SetDllImportResolver(Assembly.GetAssembly(typeof(RetroBindings))!, DllImportResolver);
+        
         // if you want to use a different Application Delegate class from "AppDelegate"
         // you can specify it here.
         UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+    
+    private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        return libraryName.Equals("melondsds_libretro") ?
+            // Flatpak runtime doesn't have libSDL2.so, so we make do
+            NativeLibrary.Load("Frameworks/melondsds_libretro.framework/melondsds_libretro", assembly, searchPath) : IntPtr.Zero;
     }
 }
