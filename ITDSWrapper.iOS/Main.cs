77dd2af9dec19618273fefbd9153c9a9c0391fbb
William <will27528@gmail.com>
2026-07-24 22:18:16 +0100

    Set up iOS build and deployment (to TestFlight) (#81)
    * feat: start iOS publishing work
    
    * Attempt 1
    
    * Add Directory.Build.props
    
    * fix: plist
    
    * feat: post build events
    
    * Switch to itds package feed
    
    * Try absolute path
    
    * Add iOS wrapper build
    
    * Also, let's upload to testflight
    
    * Don't require release version
    
    * Fixup secret names
    
    * Add stuff
    
    * Add version
    
    ---------
    
    Co-authored-by: jonko0493 <jonko0493@protonmail.com>

--- ITDSWrapper.iOS/Main.cs
+++ ITDSWrapper.iOS/Main.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Avalonia.Controls.Shapes;
+using Foundation;
+using Libretro.NET.Bindings;
 using UIKit;
 
 namespace ITDSWrapper.iOS;
@@ -7,8 +13,22 @@ public class Application
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
+        // Set up resolver path for melondsds_libretro
+        NativeLibrary.SetDllImportResolver(Assembly.GetAssembly(typeof(RetroBindings))!, DllImportResolver);
+
         // if you want to use a different Application Delegate class from "AppDelegate"
         // you can specify it here.
         UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+
+    private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        return libraryName.Equals("melondsds_libretro")
+            ?
+            // iOS needs to look in this frameworks directory
+            NativeLibrary.Load(
+                System.IO.Path.Combine(NSBundle.MainBundle.BundlePath, "Frameworks", "melondsds_libretro.framework",
+                    "melondsds_libretro"), assembly, searchPath)
+            : IntPtr.Zero;
     }
 }
