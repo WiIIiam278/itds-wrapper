ba70e027392243a2c230086c1e85ed41c798557f
jonko0493 <jonko0493@protonmail.com>
2026-08-21 08:25:34 +0900

    Fixup a few warnings

--- ITDSWrapper.Desktop/DesktopScreenReader.cs
+++ ITDSWrapper.Desktop/DesktopScreenReader.cs
@@ -58,7 +58,6 @@ public unsafe partial class DesktopScreenReader : IScreenReader
 #pragma warning restore CA1416
         return true;
 #endif
-        return false;
     }
 
     public void Speak(string text)
