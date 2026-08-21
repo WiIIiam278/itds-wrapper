7a10ef9cbe0fc62d71bde904be21cb684794266b
Jonko <69772986+jonko0493@users.noreply.github.com>
2026-08-21 19:42:38 +0900

    Auto report warnings and errors via a Discord webhook (#92)

--- ITDSWrapper/Core/LogInterpreter.cs
+++ ITDSWrapper/Core/LogInterpreter.cs
@@ -1,5 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Rest;
+using Discord.Webhook;
 using ITDSWrapper.Accessibility;
 
 namespace ITDSWrapper.Core;
@@ -13,26 +21,52 @@ public partial class LogInterpreter : IDisposable
     private const string ScreenReaderVerb = "SCREEN_READER";
     private const string StartupReceivedVerb = "STARTUP_RECEIVED";
     private const string LanguageReceivedVerb = "LANG_RECEIVED";
+    private const string WarningVerb = "WARNING";
+    private const string SaveTraceVerb = "SAVE_TRACE";
 
     public bool WatchForSdCreate { get; set; }
     public Action<string>? SetNextBorder { get; set; }
-    
+
     public IAchievementManager? AchievementManager { get; set; }
     public IScreenReader? ScreenReader { get; set; }
-    
+
     public bool StartupReceived { get; private set; }
     public bool LangReceived { get; private set; }
 
+    private readonly string? _discordWebhookUri;
+    private readonly DropOutQueue<string> _recentLogs = new(2000);
+    private readonly List<string> _saveTrace = [];
+
+    public LogInterpreter()
+    {
+        foreach (var attr in Assembly.GetExecutingAssembly().GetCustomAttributes<AssemblyMetadataAttribute>())
+        {
+            if (attr.Key == "DiscordWebhookUri")
+            {
+                _discordWebhookUri = attr.Value;
+            }
+        }
+    }
+
     public virtual int InterpretLog(string log)
     {
+        _recentLogs.Add(log);
+
+        if (log.Contains("ARM9: data abort") || log.Contains("ARM9: prefetch abort"))
+        {
+            SendWebhookLog("Abort", log).GetAwaiter().GetResult();
+            return -1;
+        }
+
         // The SD card is initialized after the game boots -- if we replace the SD card image here, it will load properly
-        int wrapperPrefixLocation = WatchForSdCreate && log.Contains("[melonDS] Game is now booting") ? 0 :
-            log.IndexOf(WrapperLogPrefix, StringComparison.Ordinal);
+        int wrapperPrefixLocation = WatchForSdCreate && log.Contains("[melonDS] Game is now booting")
+            ? 0
+            : log.IndexOf(WrapperLogPrefix, StringComparison.Ordinal);
         if (wrapperPrefixLocation < 0)
         {
             return wrapperPrefixLocation;
         }
-        
+
         int startIndex = wrapperPrefixLocation + WrapperLogPrefix.Length;
         int endIndex = log.IndexOf(':', startIndex);
         string verb = log[startIndex..endIndex];
@@ -43,25 +77,64 @@ public partial class LogInterpreter : IDisposable
             case AchievementUnlockedVerb:
                 AchievementManager?.Unlock(logParam);
                 break;
-            
+
             case BorderSetVerb:
                 SetNextBorder?.Invoke(logParam);
                 break;
-            
+
             case ScreenReaderVerb:
                 ScreenReader?.Speak(TextColorRegex().Replace(logParam, ""));
                 break;
-            
+
             case StartupReceivedVerb:
                 StartupReceived = true;
                 break;
-            
+
             case LanguageReceivedVerb:
                 LangReceived = true;
+                break;
+
+            case WarningVerb:
+                SendWebhookLog("Warning", logParam).GetAwaiter().GetResult();
+                break;
+
+            case SaveTraceVerb:
+                if (logParam.StartsWith("SCENE"))
+                {
+                    SendWebhookLog("Crash", $"In scene {logParam}").GetAwaiter().GetResult();
+                }
+
+                _saveTrace.Add(logParam);
+                if (logParam.StartsWith("DONE"))
+                {
+                    SendWebhookFile("Save Trace", $"savetrace.txt", _saveTrace).GetAwaiter().GetResult();
+                }
                 break;
         }
 
         return wrapperPrefixLocation;
+    }
+
+    private async Task SendWebhookLog(string title, string description)
+    {
+        using DiscordWebhookClient client = new(_discordWebhookUri);
+
+        EmbedBuilder embed = new();
+        embed.WithTitle(title).WithDescription(description);
+        await client.SendMessageAsync(embeds: [embed.Build()]);
+
+        await SendWebhookFile("Log", $"{title}.log", _recentLogs.GetList());
+    }
+
+    private async Task SendWebhookFile(string text, string filename, List<string> lines)
+    {
+        using DiscordWebhookClient client = new(_discordWebhookUri);
+
+        MemoryStream fileStream = new();
+        StreamWriter writer = new(fileStream);
+        await writer.WriteAsync(string.Join('\n', lines.Select(l => l.Trim())));
+        await writer.FlushAsync();
+        await client.SendFileAsync(text: text, filename: filename, stream: fileStream);
     }
 
     public void Dispose()
